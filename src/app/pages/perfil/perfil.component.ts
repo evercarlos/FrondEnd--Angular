de8371ef48a9a4ec1bb039a25b9f4f2cd7192cbc
evercarlos <ecarlosr@alumno.unsm.edu.pe>
2022-07-04 14:47:56 -0500

    add upload

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Usuario } from 'src/app/models/user.model';
+
+import { FileUploadService } from 'src/app/services/file-upload.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-perfil',
@@ -18,10 +21,13 @@ export class PerfilComponent implements OnInit {
     }
   );
   public usuario?: Usuario;
+  public imagenSubir!: File;
+  public imgTemp:any = null;
 
   constructor(
     private fb: FormBuilder,
     private usuarioService: UsuarioService,
+    private fileUploadService: FileUploadService,
   ) { 
     this.usuario = this.usuarioService.usuario;
   }
@@ -42,7 +48,38 @@ export class PerfilComponent implements OnInit {
       const {nombre, email } = this.perfilForm.value;
       this.usuario!.nombre = nombre; //  porque usamos el mismo nombre se actualiza automaticamente en todos
       this.usuario!.email = email;
+      Swal.fire('Guardado', 'Cambios fueron guardados', 'success');
+    }, (err) => {
+      Swal.fire('Error', err.error.msg, 'error');
     })
+  }
+
+  cambiarImagem(event:any){
+    this.imagenSubir = event.target.files[0];
+    
+    if(!event.target.files[0]){
+       this.imgTemp = null;
+       return;
+    } // si pasa ejecuta lo siguiente
+
+    const reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+
+    reader.onloadend = () => {
+      this.imgTemp = reader.result;//result: es el base64
+      //console.log(reader.result);
+    }
+
+  }
+
+  subirImagen() {
+    this.fileUploadService.actualizarFoto(this.imagenSubir, 'usuarios', this.usuario?.uid||'')
+    .then(img=>{
+      Swal.fire('Guardado', 'Imagen actualizado correctamente', 'success');
+      this.usuario!.img= img 
+    }, err => {
+      Swal.fire('Error', err.error.msg, 'error'); 
+    });
   }
 
 }
